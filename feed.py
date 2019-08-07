0c8e3f01ca1162846830845ed8632321af62c5ae
ruturajH <53853127+ruturajH@users.noreply.github.com>
2019-08-07 13:34:50 +0530

    Update feed.py

--- feed.py
+++ feed.py
@@ -8,6 +8,9 @@ print d.headers
 print len(d['entries'])
 #h_length
 
+print d['feed']['link']
+#f_link
+
 for post in d.entries:
     print post.title + ": " + post.link + " "
     #post and their titles/links
